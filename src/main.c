4feba3ba11a33c9544aa83d20c5f672f723ae33c
Saathvikk Muthyala <151827469+21mcme04@users.noreply.github.com>
2025-03-15 11:22:54 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,13 +8,15 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
+  while(1) {
+      // Uncomment this block to pass the first stage
+      printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  checkUserInput(input);
+      // Wait for user input
+      char input[100];
+      fgets(input, 100, stdin);
+      checkUserInput(input);
+  }
 
   return 0;
 }
