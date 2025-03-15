9227a0267fcf44fc245159d3e16586030c0969e8
Saathvikk Muthyala <151827469+21mcme04@users.noreply.github.com>
2025-03-15 11:54:52 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define validCommands 3
+char* commands[validCommands] = {"echo", "type", "exit"};
+
 void checkUserInput(char* input);
 void echo(char* input);
+void type(char* input);
+int isValidCommand(char* command);
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -37,6 +41,9 @@ void checkUserInput(char* input) {
     if(strcmp(command, "echo") == 0) {
         echo(input);
         return;
+    } else if(strcmp(command, "type") == 0) {
+        type(input);
+        return;
     }
     printf("%s: command not found\n", input);
 }
@@ -50,5 +57,29 @@ void echo(char* input) {
         argument = strtok(NULL, " ");
     }
     printf("\n");
+    return;
+}
+
+int isValidCommand(char* input) {
+    for(int i = 0; i < validCommands; i++) {
+        if(strcmp(commands[i], input) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void type(char* input) {
+    char *duplicateInput = strdup(input);
+    char* command = strtok(duplicateInput, " ");
+    char* argument = strtok(NULL, " ");
+    while(argument != NULL) {
+        if(isValidCommand(argument)) {
+            printf("%s is a shell builtin\n", argument);
+        } else {
+            printf("%s: not found\n", argument);
+        }
+        argument = strtok(NULL, " ");
+    }
     return;
 }
